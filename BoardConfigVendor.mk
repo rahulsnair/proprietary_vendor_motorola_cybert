556c285e2913581e02a16d9a0fe3d92cf265972a
rahulsnair <rahulsnair.vdp@gmail.com>
2025-12-29 12:44:50 +0530

    vendor blobs : W1VV36H.7-21-5

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,26 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    apusys \
+    audio_dsp \
+    ccu \
+    connsys_bt \
+    connsys_wifi \
+    dpm \
+    gpueb \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    modem \
+    mvpu_algo \
+    pi_img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
